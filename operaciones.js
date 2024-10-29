317cfc9b5d09fec3fe6682d213c07a12edb16a41
jainenb <jainenbanqueroalbornoz@gmail.com>
2024-10-29 17:47:10 -0300

    archivo operaciones agregado

---
+++ operaciones.js
@@ -0,0 +1,15 @@
+//Declarando las variables
+
+let numeroUno = 400;
+let numeroDos = 20;
+
+// Operaciones aritméticas y mostrando los resultados en la consola
+console.log("Suma:", numeroUno + numeroDos);
+console.log("Resta:", numeroUno - numeroDos);
+console.log ("Multiplicación:", numeroUno * numeroDos);
+console.log ("División:", numeroUno / numeroDos);
+console.log ("Modulo:", numeroUno % numeroDos);
+
+// Comparaciones de mayor y menor
+console.log("¿Es numeroUno mayor que numeroDos?", numeroUno > numeroDos);
+console.log("¿Es numeroUno menor que numeroDos?", numeroUno < numeroDos);
